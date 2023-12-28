6a74ea8cf4d2bf732d0628fc640083ad882f1e1d
mrityunjaysaraf <mrityunjaysaraf5678@gmail.com>
2023-12-29 01:00:41 +0530

    menu bar is ready

---
+++ src/component/Menu/index.tsx
@@ -0,0 +1,38 @@
+import {
+  faCoffee,
+  faEraser,
+  faFileArrowDown,
+  faPencil,
+  faRotateLeft,
+  faRotateRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./index.module.css";
+
+const Menu = () => {
+  return (
+    <div className={styles.menuContainer}>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faPencil} className={styles.icon} />
+      </div>
+
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faEraser} className={styles.icon} />
+      </div>
+
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
+      </div>
+
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
+      </div>
+
+      <div>
+        <FontAwesomeIcon icon={faFileArrowDown} />
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
