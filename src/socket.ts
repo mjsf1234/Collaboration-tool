a33e0acf90db1dbb1ffb3a837856ce9ffcc01dcb
mrityunjaysaraf <mrityunjaysaraf5678@gmail.com>
2024-01-02 00:28:39 +0530

    provide the be endPoint

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,9 @@
 import { log } from "console";
 import { io } from "socket.io-client";
-
-const socket = io("http://localhost:5001");
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://collab-server-cumf.onrender.com"
+    : "http://localhost:5001";
+const socket = io(URL);
 
 export default socket;
